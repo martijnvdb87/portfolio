f85668f984b2537acdd2b4f66d3130c48a24c5d2
martijnvdb87 <martijnvdb87@users.noreply.github.com>
2022-04-23 22:45:08 +0000

    deploy: d30fe24078b9d83d016791dd3f6c0c8bbe303ce5

---
+++ _nuxt/static/1650753907/blog/third-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1650753907",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fthird-post",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c723eb26"}}}}(null));
