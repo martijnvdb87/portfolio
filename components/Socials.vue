cb574d16f2882cadd0a3f2260f0233330634df45
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-04-24 00:11:48 +0200

    Added Socials component

---
+++ components/Socials.vue
@@ -0,0 +1,41 @@
+<template>
+<ul>
+  <li>
+    <Icon type="github" />
+  </li>
+  <li>
+    <Icon type="codepen" />
+  </li>
+  <li>
+    <Icon type="linkedin" />
+  </li>
+  <li>
+    <Icon type="instagram" />
+  </li>
+  <li>
+    <Icon type="email" />
+  </li>
+</ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Icon from '~/components/Icon.vue';
+
+export default Vue.extend({
+  name: 'Socials',
+  components: { Icon },
+});
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+}
+li {
+  @apply mr-2;
+}
+li:last-child {
+  margin-right: 0;
+}
+</style>
