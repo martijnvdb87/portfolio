12d3fc7b5bf49f9b273612a34754381375f39787
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-04-24 00:12:43 +0200

    Added header

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,32 @@
 <template>
   <App>
-    <MainMenu />
-    Home
+    <header>
+      <h1>Martijn van den Bosch</h1>
+      <p>Full Stack Developer</p>
+      <Socials />
+    </header>
+
   </App>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import App from '~/components/App.vue'
+import Vue from 'vue';
+import App from '~/components/App.vue';
+import Icon from '~/components/Icon.vue';
+import Socials from '~/components/Socials.vue';
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: { App },
-})
+  components: { App, Icon, Socials },
+});
 </script>
+
+<style scoped>
+h1 {
+  @apply text-xl font-medium text-gray-900;
+}
+
+p {
+  @apply text-gray-500 mb-8;
+}
+</style>
