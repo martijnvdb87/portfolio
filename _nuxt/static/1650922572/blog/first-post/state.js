c87eafd2198f8db083dd767c46ede74614adcbcc
martijnvdb87 <martijnvdb87@users.noreply.github.com>
2022-04-26 20:10:42 +0000

    deploy: 2a0d6b3d6abb623fe1ac02eec5d07971cff6af59

--- _nuxt/static/1650922572/blog/first-post/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1650922572",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ffirst-post",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"29a101d0"}}}}(null));
