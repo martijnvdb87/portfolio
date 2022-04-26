2a0d6b3d6abb623fe1ac02eec5d07971cff6af59
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-04-26 22:09:06 +0200

    Adde Link hover effect

--- components/Link.vue
+++ components/Link.vue
@@ -1,6 +1,13 @@
 <template>
   <a :href="href" :target="target" rel="noreferrer noopener">
-    <slot></slot>
+    <span class="link-container">
+      <span class="link-placholder" aria-hidden="true">
+        <slot></slot>
+      </span>
+      <span class="link-visible">
+        <slot></slot>
+      </span>
+    </span>
     <svg
       v-if="target == '_blank'"
       xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +53,38 @@ a {
   display: inline-flex;
   align-items: center;
   gap: 0.125em;
-  border-bottom: 1px solid #333;
+  line-height: 1.3em;
+  border-bottom: 1px solid #666;
+}
+
+.link-container {
+  position: relative;
+}
+
+.link-placholder {
+  opacity: 0;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+
+.link-visible {
+  position: absolute;
+  top: 0px;
+  left: -1em;
+  right: -1em;
+  text-align: center;
+  pointer-events: none;
+  transition: ease all 200ms;
+}
+
+a:hover .link-visible {
+  font-weight: 700;
+  letter-spacing: -0.025em;
 }
 
 svg {
