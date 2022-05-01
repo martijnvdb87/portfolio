1bf1dfb9f6a92e89507c7f44ce8d0a1ff719bfa4
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-05-01 12:08:21 +0200

    Added status update logic

--- components/Status.vue
+++ components/Status.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="status">
+  <div v-if="hasStatus" class="status">
     <span class="status__label">
       <span class="status__icon">
         <Icon :type="icon" />
+      </span>
+      <span class="status__text">
+        <label>{{ label }}</label>
       </span>
     </span>
   </div>
@@ -15,17 +18,47 @@ import Icon from './Icon.vue'
 export default Vue.extend({
   name: 'Status',
   components: { Icon },
-  data () {
+  data() {
     return {
+      hasStatus: false,
       icon: 'code',
-      label: 'label',
+      label: '',
+      statusInterval: null,
     }
+  },
+  mounted() {
+    this.getCurrentStatus()
+  },
+  beforeUnmount() {
+    clearInterval(this.statusInterval)
+  },
+  methods: {
+    getCurrentStatus() {
+      this.updateStatus()
+      this.statusInterval = setInterval(() => {
+        this.updateStatus()
+      }, 60000)
+    },
+    updateStatus() {
+      if (this.hasStatus) {
+        setTimeout(() => {
+          const statusTextLabelElement = this.$el.querySelector(
+            '.status__text label'
+          )
+          this.$el.style.setProperty(
+            '--status-width',
+            `${statusTextLabelElement.offsetWidth}px`
+          )
+        })
+      }
+    },
   },
 })
 </script>
 
 <style scoped>
 .status {
+  --status-width: 200px;
   @apply relative flex items-center justify-center h-8 w-8 rounded-full font-semibold;
 }
 
@@ -36,16 +69,21 @@ export default Vue.extend({
 .status__text {
   @apply ease-in-out duration-300 delay-200;
   max-width: 0px;
+  opacity: 0;
   overflow: hidden;
+  transform: translateX(-10px);
 }
 
 .status:hover .status__text {
   transition-delay: 0ms;
-  max-width: 250px;
+  opacity: 1;
+  max-width: var(--status-width);
+  transform: none;
 }
 
 label {
   @apply pr-4 pl-1;
+  white-space: nowrap;
 }
 
 .status__icon {
