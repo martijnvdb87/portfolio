a9f74554d8510545ab2e141d58f3f8f22e62e08d
martijnvdb87 <martijnvdb87@users.noreply.github.com>
2022-05-05 22:03:27 +0000

    deploy: 1dd3ad8229401e892d07335084150292d2e1fa09

--- _nuxt/static/1651701928/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651701928",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0064b286"}}}}(null,"\u002F"));
