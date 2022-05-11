22a8e4b017e9d318df6489b187c748fbc0b5d5cf
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-05-12 00:39:53 +0200

    Added palette and x icons

--- components/Icon.vue
+++ components/Icon.vue
@@ -119,6 +119,20 @@
       <circle cx="10" cy="10" r="7"></circle>
       <line x1="21" y1="21" x2="15" y2="15"></line>
     </template>
+
+    <template v-else-if="type == 'palette'">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 21a9 9 0 1 1 0 -18a9 8 0 0 1 9 8a4.5 4 0 0 1 -4.5 4h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
+      <circle cx="7.5" cy="10.5" r=".5" fill="currentColor"></circle>
+      <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+      <circle cx="16.5" cy="10.5" r=".5" fill="currentColor"></circle>
+    </template>
+
+    <template v-else-if="type == 'x'">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </template>
   </svg>
 </template>
 
