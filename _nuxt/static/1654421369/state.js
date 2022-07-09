6bfd72ee44e1eb2cd2df252a9c405631275db816
martijnvdb87 <martijnvdb87@users.noreply.github.com>
2022-07-09 14:03:06 +0000

    deploy: d86d8c5fad8f473854af80db3b2b5291bd91641b

--- _nuxt/static/1654421369/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654421369",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1366ae44"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1657375386/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657375386",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b9be9c1a"}}}}(null));
