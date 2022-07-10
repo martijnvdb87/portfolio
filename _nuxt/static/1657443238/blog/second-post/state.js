778320a4107a3a89f630d8744ddf17cc6a261d14
martijnvdb87 <martijnvdb87@users.noreply.github.com>
2022-07-10 08:53:59 +0000

    deploy: 6863daeac8b6783d750a36f552dd78213fbe9576

---
+++ _nuxt/static/1657443238/blog/second-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657443238",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fsecond-post",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"85a54d43"}}}}(null));
